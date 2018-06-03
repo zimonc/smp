8c3cd004c6d4502a560b9bf531352a61ca5d9b35
Simone Carcangiu <simone.carcangiu@gmail.com>
2018-06-03 14:01:22 +0200

    Implemented and unit-tested SaleDao.createOrUpdate.
    Added to SaleDao a memory field who is a list of SaleEntity objecys.
    Also added two constructors who both initialize that field.

--- src/main/java/com/jpm/test/smp/dao/SaleDaoInMemory.java
+++ src/main/java/com/jpm/test/smp/dao/SaleDaoInMemory.java
@@ -1,15 +1,36 @@
 package com.jpm.test.smp.dao;
 
 import com.jpm.test.smp.entity.SaleEntity;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SaleDaoInMemory implements SaleDao
 {
+    private List<SaleEntity> memory;
 
-    @Override
+    public SaleDaoInMemory()
+    {
+        this.memory = new ArrayList<>();
+    }
+
+    public SaleDaoInMemory(List<SaleEntity> memory)
+    {
+        this.memory = memory;
+    }
+
     public SaleEntity createOrUpdate(SaleEntity saleEntity)
     {
-        return null;
+        if (!memory.contains(saleEntity))
+        {
+            saleEntity.setTimestamp(LocalDateTime.now());
+            memory.add(saleEntity);
+        }
+        else
+        {
+            // We have do do nothing since the object was, eventually, updated "in place".
+        }
+        return saleEntity;
     }
 
 
--- src/test/java/com/jpm/test/smp/dao/SaleDaoInMemoryTest.java
+++ src/test/java/com/jpm/test/smp/dao/SaleDaoInMemoryTest.java
@@ -1,14 +1,33 @@
 package com.jpm.test.smp.dao;
 
+import com.jpm.test.smp.entity.SaleEntity;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
 
 class SaleDaoInMemoryTest
 {
+
     @Test
     void createOrUpdate()
     {
+        List<SaleEntity> memoryMock = Mockito.mock(List.class);
+        SaleDao saleDao = new SaleDaoInMemory(memoryMock);
+
+        SaleEntity saleEntity = new SaleEntity("apple", 10.2, 3);
+        SaleEntity saleEntityStored = saleDao.createOrUpdate(saleEntity);
+
+        Assertions.assertEquals(saleEntity, saleEntityStored);
+        Assertions.assertNotNull(saleEntityStored.getTimestamp());
+        System.out.println(saleEntityStored.getTimestamp());
+        System.out.println(LocalDateTime.now());
+        Assertions.assertTrue(LocalDateTime.now().isAfter(saleEntityStored.getTimestamp()));
+
+        Mockito.verify(memoryMock, Mockito.times(1)).add(saleEntity);
     }
 
 
