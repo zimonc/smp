0fde9074b4673df977d2623975b45d1c908813c7
Simone Carcangiu <simone.carcangiu@gmail.com>
2018-06-03 14:59:46 +0200

    Added unit-test before implementing SaleServiceDefaule.findByProductType method
    (i.e. TDD way)

--- src/test/java/com/jpm/test/smp/service/SaleServiceDefaultTest.java
+++ src/test/java/com/jpm/test/smp/service/SaleServiceDefaultTest.java
@@ -2,6 +2,8 @@ package com.jpm.test.smp.service;
 
 import com.jpm.test.smp.dao.SaleDao;
 import com.jpm.test.smp.entity.SaleEntity;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
@@ -26,8 +28,18 @@ class SaleServiceDefaultTest
     @Test
     void findByProductType()
     {
-    }
+        SaleDao saleDaoMock = Mockito.mock(SaleDao.class);
+        SaleService saleService = new SaleServiceDefault(saleDaoMock);
+        String productType = "any";
+        List<SaleEntity> saleEntityList = Arrays.asList(
+            new SaleEntity("apple", 12.5, 1),
+            new SaleEntity("nut", 12.3, 2));
+        Mockito.when(saleDaoMock.findByProductType(productType)).thenReturn(saleEntityList);
 
+        Assertions.assertEquals(saleEntityList, saleService.findByProductType(productType));
+
+        Mockito.verify(saleDaoMock, Mockito.times(1)).findByProductType(productType);
+    }
 
     @Test
     void adjust()
